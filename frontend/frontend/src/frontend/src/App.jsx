c4fb8c84d13712e2e8336fc9315a897e5cb550ce
rounaquesparsh-rgb <rounaquesparsh@gmail.com>
2025-11-17 20:15:34 +0530

    Create App.jsx

---
+++ frontend/frontend/src/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import AuthCallback from './pages/AuthCallback'
+
+export default function App(){
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/auth/callback" element={<AuthCallback/>} />
+    </Routes>
+  )
+}
